191a5b527b0e5b93f86c48933af7caf3ed5d2eaf
Piotr Studziński <studniapl@interia.pl>
2022-05-22 13:42:56 +0200

    Changing communication from udp to tcp

--- chat-gui/CreateChatWindow.py
+++ chat-gui/CreateChatWindow.py
@@ -17,20 +17,10 @@ class CreateChatWindow:
         self.cipherMode.set(self.cipherOptions[0])
         self.cipherModeOptionMenu = tk.OptionMenu(self.root, self.cipherMode, *self.cipherOptions)
 
-        # # Listener port
-        # self.listenPortLabel = tk.Label(self.root, text="Enter listener port:")
-        # self.listenPortEntry = tk.Entry(self.root)
-        # self.listenPortEntry.insert(tk.END, '50001')
-
-        # Sender port
-        # self.sendPortLabel = tk.Label(self.root, text="Enter sender port:")
-        # self.sendPortEntry = tk.Entry(self.root)
-        # self.sendPortEntry.insert(tk.END, '50002')
-
         # Destination IP
         self.ipLabel = tk.Label(self.root, text="Enter destination IP:")
         self.ipEntry = tk.Entry(self.root)
-        self.ipEntry.insert(tk.END, '172.20.72.211')
+        self.ipEntry.insert(tk.END, '127.0.0.1')
 
         # Destination port
         self.destPortLabel = tk.Label(self.root, text="Enter destination port:")
@@ -43,10 +33,6 @@ class CreateChatWindow:
 
         self.cipherModeLabel.grid(row=0, column=0)
         self.cipherModeOptionMenu.grid(row=0, column=1)
-        # self.listenPortLabel.grid(row=1, column=0)
-        # self.listenPortEntry.grid(row=1, column=1)
-        # self.sendPortLabel.grid(row=2, column=0)
-        # self.sendPortEntry.grid(row=2, column=1)
         self.ipLabel.grid(row=3, column=0)
         self.ipEntry.grid(row=3, column=1)
         self.destPortLabel.grid(row=4, column=0)
@@ -55,16 +41,17 @@ class CreateChatWindow:
 
     def startChat(self):
         print("Cipher mode:\t" + self.cipherMode.get())
-        # print("Listener port:\t" + self.listenPortEntry.get())
-        # print("Sender port:\t" + self.sendPortEntry.get())
         print("IP Address:\t" + self.ipEntry.get())
         print("Destination port:\t" + self.destPortEntry.get())
 
-        # self.parent.senderSocket.bind(('', int(self.sendPortEntry.get())))
         self.parent.destIP = self.ipEntry.get()
         self.parent.destPort = int(self.destPortEntry.get())
-        #msg = 'Polaczenie zostalo utworzone!'
         msg = f'{self.parent.listenerPort} ; Polaczenie zostalo utworzone!'
-        self.parent.senderSocket.sendto(msg.encode(), (self.parent.destIP, self.parent.destPort))
+       
+        senderSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
+        senderSocket.connect(('127.0.0.1', self.parent.destPort))
+        senderSocket.sendall(msg.encode())
+        senderSocket.close()
+
         self.root.destroy()
--- chat-gui/MainWindow.py
+++ chat-gui/MainWindow.py
@@ -13,8 +13,6 @@ class MainWindow:
         self.listenerPort = int(listenerPort)
         self.senderPort = int(senderPort)
 
-        self.senderSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.senderSocket.bind(('', self.senderPort))
         self.destIP = ""
         self.destPort = -1
 
@@ -67,7 +65,16 @@ class MainWindow:
         self.chatTextBox.insert(tk.INSERT, msg)
         self.chatTextBox.config(state=tk.DISABLED)
 
-        self.senderSocket.sendto(msg.encode(), (self.destIP, self.destPort))
+
+        senderSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("jestesmy w sendMessage")
+        print("send1",self.destIP)
+        print("send2",self.destPort)
+        
+        senderSocket.connect((self.destIP,self.destPort))
+        senderSocket.send(msg.encode())
+
+        senderSocket.close()
 
     def showMessage(self, msg):
         self.chatTextBox.config(state=tk.NORMAL)
@@ -80,23 +87,28 @@ class MainWindow:
         self.chatTextBox.config(state=tk.DISABLED)
 
     def listenFunction(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.bind(('', self.listenerPort))
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind(('127.0.0.1', self.listenerPort))
+        sock.listen()
+
 
         while True:
-            data, senderInfo = sock.recvfrom(128)
-            print("data",data)
-            print("senderInfo",senderInfo)
-            msg = data.decode()
-            if self.destPort == -1:
-                senderAddr, senderPort = senderInfo
-                print("senderAddr",senderAddr)
-                print("senderPort",senderPort)
-                self.destIP = senderAddr
-                self.destPort = int(msg.split(";")[0])
-                # port, message = msg.split(";")
-                # print("destIP", port)
-                # self.destIP = port
-                self.showMessage(msg.split(";")[1])
-            # print('\rpeer: {}\n> '.format(data.decode()), end='')
-            self.showMessage(msg)
+            conn, senderInfo = sock.accept()
+            print("Listener Function")
+            print("conn", conn)
+            print("senderInfo", senderInfo)
+
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                msg = data.decode()
+                #print(msg)
+                if self.destPort == -1:
+                    senderAddr, senderPort = senderInfo
+                    print("senderAddr",senderAddr)
+                    print("senderPort",senderPort)
+                    self.destIP = senderAddr
+                    self.destPort = int(msg.split(";")[0])
+                self.showMessage(msg)
+            conn.close()
